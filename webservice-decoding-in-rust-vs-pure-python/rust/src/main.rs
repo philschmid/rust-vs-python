a874a1e40f988d176fc169250262e796472aa522
philschmid <schmidphilipp1995@gmail.com>
2022-01-05 23:09:18 +0000

    added first cpu tests

--- webservice-decoding-in-rust-vs-pure-python/rust/src/main.rs
+++ webservice-decoding-in-rust-vs-pure-python/rust/src/main.rs
@@ -1,59 +1,55 @@
-use std::time::Instant;
-
 use actix_web::web::Data;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
 use pyo3::prelude::{Py, PyAny, PyModule, Python};
-use serde::Deserialize;
+use pyo3::FromPyObject;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 struct RequestData {
     inputs: String,
 }
+#[derive(FromPyObject, Debug, Serialize)]
+struct TextClassificationResponse {
+    #[pyo3(item)]
+    label: String,
+    #[pyo3(item)]
+    score: f32,
+}
 
 /// deserialize `Info` from request's body, max payload size is 4kb
-async fn index(
-    // py: Data<Python>,
-    text_classification: Data<Py<PyAny>>,
-    r: web::Json<RequestData>,
-) -> HttpResponse {
+async fn index(text_classification: Data<Py<PyAny>>, r: web::Json<RequestData>) -> HttpResponse {
     println!("{}", r.inputs);
-    // println!("{:?}",counter2);
-    let now = Instant::now();
-    let gil = Python::acquire_gil();
-    let py = gil.python();
-    println!("Loading python with gil {}", now.elapsed().as_millis());
-
-    // let pred = text_classification.call1(py, ("i love you.",)).unwrap();
-    let pred = text_classification
-        .call1(gil.python(), ("i love you.",))
-        .unwrap();
-
-    // println!("{:?}", pred);
+    let pred = Python::with_gil(|py| {
+        text_classification
+            .call1(py, ("i love you.",))
+            .unwrap()
+            .extract::<Vec<TextClassificationResponse>>(py)
+            .unwrap()
+    });
+    println!("{:?}", pred);
 
     HttpResponse::Ok()
         .content_type("application/json")
-        .body("{\"test\":\"success\"}")
+        .json(pred)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let gil = Python::acquire_gil();
-    let transformers = PyModule::import(gil.python(), "transformers").unwrap();
-    let text_classification: Data<Py<PyAny>> = Data::new(
-        transformers
+    let text_classification: Py<PyAny> = Python::with_gil(|py| {
+        let transformers = PyModule::import(py, "transformers").unwrap();
+        return transformers
             .getattr("pipeline")
             .unwrap()
             .call1(("text-classification",))
             .unwrap()
             .extract()
-            .unwrap(),
-    );
+            .unwrap();
+    });
 
     HttpServer::new(move || {
         App::new()
-            // .app_data() // add thread-local state
-            .app_data(text_classification.clone()) // add thread-local state
+            .app_data(Data::new(text_classification.clone())) // add thread-local state
             .service(
                 web::resource("/age")
                     // change json extractor configuration
